c944befd9be902975b653f45c9e9456542997987
kenderb <kenderbolivar@gmail.com>
2020-12-21 17:26:44 -0500

    Add linters to the project

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, author, pageNumber, status) {
   this.title = title;
@@ -8,22 +8,23 @@ function Book(title, author, pageNumber, status) {
 }
 
 Book.prototype.showBook = function () {
-  return {title: this.title,
-          author: this.author,
-          pageNumber: this.pageNumber,
-          status: this.status
-  }
-}
+  return {
+    title: this.title,
+    author: this.author,
+    pageNumber: this.pageNumber,
+    status: this.status,
+  };
+};
 Book.prototype.isValid = function () {
   if (this.title && this.author && this.pageNumber) return true;
   return false;
-}
+};
 function getBookInfo() {
-  const bookTitle = document.querySelector("#Title").value;
-  const authorName = document.querySelector("#Author").value;
-  const pages = document.querySelector("#numberPage").value;
-  const isRead = document.querySelector("#Read").checked;
-  let newBook = new Book(bookTitle, authorName, pages, isRead);
+  const bookTitle = document.querySelector('#Title').value;
+  const authorName = document.querySelector('#Author').value;
+  const pages = document.querySelector('#numberPage').value;
+  const isRead = document.querySelector('#Read').checked;
+  const newBook = new Book(bookTitle, authorName, pages, isRead);
   console.log(newBook);
   if (!newBook.isValid()) {
     console.log('All field required');
@@ -35,5 +36,5 @@ function getBookInfo() {
 function addBookToLibrary() {
 
 }
-const addBtn = document.querySelector("#Add");
-addBtn.addEventListener("click", getBookInfo);
+const addBtn = document.querySelector('#Add');
+addBtn.addEventListener('click', getBookInfo);
