ad617b97278179a56c04cc86c6d57fddf61954a8
kenderb <40863532+kenderb@users.noreply.github.com>
2020-12-22 11:59:48 -0500

    Render Books to the DOM

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,9 @@
-const myLibrary = [];
+const myLibrary = [{
+  title: 'The book',
+  author: 'The author',
+  pageNumber: '900',
+  status: false,
+}];
 
 function Book(title, author, pageNumber, status) {
   this.title = title;
@@ -10,29 +15,57 @@ function Book(title, author, pageNumber, status) {
 Book.prototype.showBook = function showBook() {
   return {
     title: this.title,
-    author: this.author,
+    author: this.title,
     pageNumber: this.pageNumber,
     status: this.status,
   };
 };
 
 Book.prototype.isValid = function isValid() {
-  let messages = [];
-  if(this.title === "" || this.title == null){
+  const messages = [];
+  if (this.title === '' || this.title == null) {
     messages.push('Title is missing.');
   }
-  if(this.author === "" || this.author == null){
+  if (this.author === '' || this.author == null) {
     messages.push('Author is missing.');
   }
-  if (this.pageNumber === "" || this.pageNumber == null || Number(this.pageNumber) < 1 ) {
+  if (this.pageNumber === '' || this.pageNumber == null || Number(this.pageNumber) < 1) {
     messages.push('pages cannot be less than 1.');
   }
 
-  for(let item of myLibrary){
-    if(item.title == this.title) messages.push('This book already exist');
-  }
+  myLibrary.forEach(element => {
+    if (element.title === this.title) {
+      messages.push('This book already exist');
+    }
+  });
+
   return messages;
 };
+
+function displayBooks() {
+  const booksContainer = document.querySelector('.books-container');
+  booksContainer.innerHTML = '';
+  myLibrary.forEach(element => {
+    const cardContainer = document.createElement('div');
+    cardContainer.className = 'card col-4 p-2';
+    cardContainer.innerHTML = `<div class="card-body">
+                              <h5 class="card-title">${element.title}</h5>
+                              <p class="card-text">${element.author}</p>
+                              <p class="card-text">${element.pageNumber}</p>
+                              <a href="#" class="btn btn-danger">delete</a>
+                              </div>`;
+    booksContainer.appendChild(cardContainer);
+  });
+  return true;
+}
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+  if (myLibrary.length > 0) {
+    displayBooks();
+  }
+  return myLibrary;
+}
 
 function getBookInfo() {
   const bookTitle = document.querySelector('#Title').value;
@@ -42,18 +75,14 @@ function getBookInfo() {
   const newBook = new Book(bookTitle, authorName, pages, isRead);
   const error = document.getElementById('error');
   const listOfErrors = newBook.isValid();
-  if(listOfErrors.length > 0){
-    error.innerHTML = listOfErrors.join("</br>");
+  if (listOfErrors.length > 0) {
+    error.innerHTML = listOfErrors.join('</br>');
     return false;
   }
-  myLibrary.push(newBook);
-  return myLibrary;
-}
-
-function addBookToLibrary() {
+  addBookToLibrary(newBook);
   return true;
 }
-console.log(myLibrary);
+
 const addBtn = document.querySelector('#Add');
 addBtn.addEventListener('click', getBookInfo);
-addBookToLibrary();
+displayBooks();
